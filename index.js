b5b5b3c36de0f77ce3d8f31d100a0c7a694302cb
JakeThomson <jakethomson0@gmail.com>
2021-03-05 18:03:59 +0000

    Enabled CORS for localhost & jake-t.codes. Socket connection is now established with front-end and date change events are emitted.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const socket = require('socket.io');
 const moment = require('moment');
 const packageJson = require('./package.json');
 
 const app = express();
 const port = 8080;
+
+const server = app.listen(port, () => {
+  console.log(`App server now listening to port ${port}`);
+});
+
+const io = socket(server, { cors: { origin: ['http://localhost:3000', 'https://algo-trader.jake-t.codes'], } });
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(express.static('public'));
+app.use(function(req, res, next) {
+  const allowedOrigins = ['http://localhost:3000', 'https://algo-trader.jake-t.codes'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  };
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Connect to database using env variables.
 const pool = mysql.createPool({
@@ -18,13 +36,17 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DB,
 });
 
-const server = app.listen(port, () => {
-  console.log(`App server now listening to port ${port}`);
-});
-
 // Listen for GET requests to '/' and return welcome message with API version number.
 app.get('/', (req, res) => {
   res.send(`Jake's trading api (version: ${packageJson.version})`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 // Listen for PUT requests to /backtest_properties/initialise and re-initialise the backtest properties.
@@ -65,12 +87,12 @@ app.put('/backtest_properties/initialise', (req, res) => {
 	});
 })
 
-// Listen for PUT requests to /backtest_properties/initialise and re-initialise the backtest properties.
+// Listen for PATCH requests to /backtest_properties/date to set a new date value in the backtest.
 app.patch('/backtest_properties/date', (req, res) => {
   // Extract data from request body.
   const { backtest_date: backtestDate } = req.body;
 
-	// Query constructor to update the backtest properties.
+	// Query constructor to update the backtest date.
 	pool.query(`
 		UPDATE backtest_properties
 		  SET
@@ -83,7 +105,30 @@ app.patch('/backtest_properties/date', (req, res) => {
 				console.warn(new Date(), err);
 			} else {
 				// Valid and successful request.
-				res.send(row);
+        res.send(row);
+        // Send the new date as an event to the socket connection.
+        formattedDate = moment(backtestDate).format('DD/MM/YYYY')
+        payload = { backtestDate: formattedDate }
+        io.sockets.emit("dateUpdated", payload);
+			}
+	});
+})
+
+// Listen for GET requests to /backtest_properties/date to get the current date in the backtest.
+app.get('/backtest_properties/date', (req, res) => {
+
+	// Query constructor to get the current the backtest date.
+	pool.query(`SELECT backtest_date FROM backtest_properties;`, (err, row) => {
+			if(err) {
+				// If the MySQL query returned an error, pass the error message onto the client.
+				res.status(500).send({devErrorMsg: err.sqlMessage, clientErrorMsg: "Internal server error."});
+				delete err.stack;
+				console.warn(new Date(), err);
+			} else {
+				// Valid and successful request, return the formatted date within an object.
+        data = row[0]
+        data.backtest_date = moment(data.backtest_date).format('DD/MM/YYYY')
+				res.send(data);
 			}
 	});
 })
